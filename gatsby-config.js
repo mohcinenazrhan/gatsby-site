735f88cde6675f6690fef0ac501ea701cdb6b57e
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-12-08 10:38:46 +0100

    chore: config gatsby to resolve content files

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,19 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
       },
     },
     `gatsby-transformer-sharp`,
