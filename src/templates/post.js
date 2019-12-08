bfe2b75e8cd1237517c46c6b4945687769303fed
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-12-08 20:02:39 +0100

    feat: add link in the post template to navigate to the latest posts

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Link from 'gatsby-link'
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
@@ -17,6 +18,8 @@ export default function Template({ data }) {
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
         />
+        <hr />
+        <Link to="/blog">Latest Posts</Link>
       </div>
     </Layout>
   )
